8af4218925b4baeb092afd418beb0bb8c75c79f3
matisagardia <matisagardia7@gmail.com>
2022-12-11 19:59:29 -0300

    reset and highscore buttons

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 'use strict';
 
 // Generate a random number
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+let number = Math.trunc(Math.random() * 20) + 1;
+
 
 
 // Generate the score value to interact based on the correct choices
 let score = 20;
+let highScore = 0;
 
 // Event listener for the check button, which triggers all the game
 
 let checkBtn = document.querySelector(".check");
+
 checkBtn.addEventListener("click", () => {
     let guess = Number(document.querySelector(".guess").value);
 
@@ -22,6 +24,11 @@ checkBtn.addEventListener("click", () => {
         document.querySelector(".message").textContent = "👍 Correct number!";
         document.querySelector("body").style.backgroundColor = "#60b347";
         document.querySelector(".number").style.width = "30rem";
+        document.querySelector(".number").textContent = number;
+        if(score > highScore){
+        highScore = score;
+        document.querySelector(".highscore").textContent = highScore;
+        }
         // If the number is not valid
     } else if(guess < 0 || guess > 20){
         document.querySelector(".message").textContent = "The number has to be higher than 0 and lower than 20.";
@@ -48,3 +55,22 @@ checkBtn.addEventListener("click", () => {
     }
 });
 
+let againBtn = document.querySelector(".again");
+
+againBtn.addEventListener("click", () => {
+    score = 20;
+    document.querySelector(".score").textContent = score;
+    number = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".guess").value = '';
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector("body").style.backgroundColor = "#222";
+})
+
+let resetHighScore = document.querySelector(".highscr");
+
+resetHighScore.addEventListener("click", () =>{
+    highScore = 0;
+    document.querySelector(".highscore").textContent = highScore;
+})
